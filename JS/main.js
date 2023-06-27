7ca2c756f996973907fa41343b77907f28e07834
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-06-27 23:36:34 +0700

    4

--- JS/main.js
+++ JS/main.js
@@ -40,25 +40,59 @@ class Phone {
         this.price = price
         this.img = img
     }
-    showProduct() {
+    pushProduct() {
         return `
-        <div style="float:left ; ">
+        <div style="float:left ;">
              <img src="${this.img}" alt="${this.name}" style="height: 300px; width: 350px;">
              <p>${this.name}</p> 
-             <p>${this.brand}</p>
+             
              <p>${this.price}</p>
          </div>
          `
     }
-}
-let Product1 = new Phone('IPHONE 14 Pro Max','iphone',26990000, '/img/iphone/iphone-14-pro-max-vang-1.jpg')
-let Product2 = new Phone('IPHONE 14 Pro','iphone',24990000, '/img/iphone/iphone-14-pro-tim-1-1.jpg')
-let Product3 = new Phone('IPHONE 14 Plus','iphone',22990000, '/img/iphone/iphone-14-plus-do-5.jpg')
 
+}
+let Product1 = new Phone('IPHONE 14 Pro Max'
+    ,'iphone',26990000, '/img/iphone/iphone-14-pro-max-vang-1.jpg')
+let Product2 = new Phone('IPHONE 14 Pro'
+    ,'phone',24990000, '/img/iphone/iphone-14-pro-tim-1-1.jpg')
+let Product3 = new Phone('IPHONE 14 Plus'
+    ,'phone',22990000, '/img/iphone/iphone-14-plus-do-5.jpg')
 let listProduct = [Product1,Product2,Product3]
-var show =''
+let show =''
 for (let i = 0; i< listProduct.length; i++) {
     let list = listProduct[i]
-    show += list.showProduct()
+    var isPhone = listProduct[i].brand
+    show += list.pushProduct()
 }
-document.getElementById('display').innerHTML = show;
+function showProduct() {
+    document.getElementById('display').innerHTML = show;
+}
+function showPhoneProducts() {
+    let phoneProducts = '';
+    for (let i = 0; i < listProduct.length; i++) {
+        if (listProduct[i].getBrand() === 'phone') {
+            phoneProducts += listProduct[i].pushProduct();
+        }
+    }
+    document.getElementById('display').innerHTML = phoneProducts;
+}
+function phone() {
+    showPhoneProducts()
+}
+
+// function phone() {
+//     if (isPhone === 'phone') {
+//     showProduct()
+//     }
+// }
+// function mac() {
+//
+// }
+// function tablet() {
+//
+// }
+// function fix() {
+//
+// }
+
